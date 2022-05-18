d4755e85a1b669c909b7b578b321c0159a58321c
quanh21 <le.quocanhh21@gmail.com>
2022-05-18 16:41:57 +0700

    Add project files.

---
+++ Demo_Microservice/Controllers/WeatherForecastController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo_Microservice.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly WeatherClient _client;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger,WeatherClient client)
+        {
+            _logger = logger;
+            _client = client;
+        }
+
+        [HttpGet]
+        [Route("{city}")] // /weather/seattle
+        public async Task<WeatherForecast> Get(string city)
+        {
+            var forecast = await _client.GetCurrentWeatherAsync(city);
+
+            return new WeatherForecast
+            {
+                Summary = forecast.weather[0].description,
+                TemperatureC = (int)forecast.main.temp,
+                Date = DateTimeOffset.FromUnixTimeSeconds(forecast.dt).DateTime
+            };
+        }
+    }
+}
---
+++ Demo_Microservice/WeatherClient.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Demo_Microservice
+{
+    public class WeatherClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ServiceSettings _settings; 
+
+        public WeatherClient(HttpClient httpClient, IOptions<ServiceSettings> options)
+        {
+            this._httpClient = httpClient;
+            this._settings = options.Value;
+        }
+
+        public record Weather(string description);
+
+        public record Main(decimal temp);
+
+        public record Forecast (Weather[] weather,Main main,long dt);
+
+        public async Task<Forecast> GetCurrentWeatherAsync(string city)
+        {
+            var forecast = await _httpClient.GetFromJsonAsync<Forecast>($"https://{_settings.OpenWeatherHost}/data/2.5/weather?q={city}&appid={_settings.ApiKey}");
+            return forecast;
+        }
+    }
+}
